1064053a739eeea4dc2bf2e3cdfe23e429443605
Faith Korir <faithckorir@gmail.com>
2020-06-06 02:01:08 +0300

    used a function to create widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,32 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
+
           child: Column(
+
+
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                child: FlatButton(
-                  color: Colors.black,
+              buildKey(Colors.teal),
+              buildKey(Colors.blue),
+              buildKey(Colors.grey),
+              buildKey(Colors.green),
+              buildKey(Colors.red),
+              buildKey(Colors.teal),
+              buildKey(Colors.orange),
 
-                  onPressed: () {
-                    playsound(1);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.lightBlueAccent,
-
-                  onPressed: () {
-                    playsound(2);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.green,
-
-                  onPressed: () {
-                    playsound(3);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.grey,
-
-                  onPressed: () {
-                    playsound(4);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.pink,
-
-                  onPressed: () {
-                    playsound(5);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.red,
-
-                  onPressed: () {
-
-                    playsound(6);
-                  },
-                ),
-              ),
-              Container(
-                child: FlatButton(
-                  color: Colors.teal,
-
-                  onPressed: () {
-                    playsound(7);
-                  },
-                ),
-              ),
 
 
 
@@ -85,8 +36,22 @@ class XylophoneApp extends StatelessWidget {
       ),
     );
   }
-  void playsound(int number){
+  void playsound({int number}){
     final player=new AudioCache();
     player.play('note$number.wav');
+  }
+  Widget buildKey(Colors){
+return
+    Expanded(
+      child: Container(
+        child: FlatButton(
+          color: Colors,
+
+          onPressed: () {
+            playsound(number: 7);
+          },
+        ),
+      ),
+    );
   }
 }
