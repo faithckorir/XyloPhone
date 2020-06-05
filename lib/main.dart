37467ddd9ed85f9116906a9d81e43e282435d0aa
Faith Korir <faithckorir@gmail.com>
2020-06-06 01:04:12 +0300

    music files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +10,83 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: FlatButton(
+                  color: Colors.black,
+
+                  onPressed: () {
+                    playsound(1);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.lightBlueAccent,
+
+                  onPressed: () {
+                    playsound(2);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.green,
+
+                  onPressed: () {
+                    playsound(3);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.grey,
+
+                  onPressed: () {
+                    playsound(4);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.pink,
+
+                  onPressed: () {
+                    playsound(5);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.red,
+
+                  onPressed: () {
+
+                    playsound(6);
+                  },
+                ),
+              ),
+              Container(
+                child: FlatButton(
+                  color: Colors.teal,
+
+                  onPressed: () {
+                    playsound(7);
+                  },
+                ),
+              ),
+
+
+
+            ],
+          ),
         ),
       ),
     );
+  }
+  void playsound(int number){
+    final player=new AudioCache();
+    player.play('note$number.wav');
   }
 }
